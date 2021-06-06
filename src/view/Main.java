18e42e62c620fb60ae70cd6275b3b00ec899460a
sarango25 <arangosebastian2001@gmail.com>
2021-06-05 23:29:40 -0500

    ejercicio incompleto

--- src/view/Main.java
+++ src/view/Main.java
@@ -31,4 +31,4 @@ public class Main extends PApplet {
 	}
 }
 
-//Profe de antemano me disculpo, realmente después de muchos intentos no pude desarrollar efectivamente el parcial. Sin embargo no queria dejar la entrega en blanco así que te adjunto el análisis previo que hice para entender un poco la lógica del procedimient. Me enredé un poco con el tema de los hilos.
+//Profe de antemano me disculpo, realmente después de muchos intentos no pude desarrollar efectivamente el parcial. Sin embargo no queria dejar la entrega en blanco así que te adjunto el análisis previo que hice para entender un poco la lógica del procedimient.
